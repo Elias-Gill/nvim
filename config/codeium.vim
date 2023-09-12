c67daf988db2fd0d41397e4a93f6f2674a38aaae
Elias Gill <eliasgill42@gmail.com>
2023-09-12 13:38:26 -0400

    cleaning config

--- config/codeium.vim
+++
@@ -1,4 +0,0 @@
-let g:codeium_disable_bindings = 1
-" mappings
-imap <script><silent><nowait><expr> <C-l> codeium#Accept()
-imap <C-x>   <Cmd>call codeium#Clear()<CR>
--- lua/elias/plugins/init.lua
+++ lua/elias/plugins/init.lua
@@ -7,7 +7,11 @@ return {
         "Exafunction/codeium.vim",
         cmd = "Codeium",
         config = function()
-            vim.cmd('source $HOME/.config/nvim/config/codeium.vim')
+            vim.cmd([[
+                let g:codeium_disable_bindings = 1
+                imap <script><silent><nowait><expr> <C-l> codeium#Accept()
+                imap <C-x>   <Cmd>call codeium#Clear()<CR>
+                ]])
         end
     },
 
