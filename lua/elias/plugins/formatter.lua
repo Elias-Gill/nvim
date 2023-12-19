e420d140e06bf18b6e962e4ab51a72308d3df27d
Elias Gill <eliasgill42@gmail.com>
2023-12-19 09:40:20 -0300

    solving linter errors

--- lua/elias/plugins/formatter.lua
+++ lua/elias/plugins/formatter.lua
@@ -25,6 +25,7 @@ return {
 			-- Use a sub-list to run only the first available formatter
 			javascript = { { "prettierd", "prettier" } },
 			typescript = { { "prettierd", "prettier" } },
+			typescriptreact = { { "prettierd", "prettier" } },
 			javascriptreact = { { "prettierd", "prettier" } },
 			astro = { { "prettierd", "prettier" } },
 		},
