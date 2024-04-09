7f601e97ef5ab712327758d07cba9244d7443b09
Elias Gill <eliasgill42@gmail.com>
2024-04-09 03:20:41 -0400

    no se, kk

--- lua/elias/plugins/devtools/copilot.lua
+++ lua/elias/plugins/devtools/copilot.lua
@@ -11,31 +11,6 @@ return {
 		end,
 	},
 
-	--[[ -- add this to the file where you setup your other plugins:
-	{
-		"monkoose/neocodeium",
-		cmd = "NeoCodeium",
-		config = function()
-			local neocodeium = require("neocodeium")
-			neocodeium.setup()
-			vim.keymap.set("i", "<A-f>", function()
-				require("neocodeium").accept()
-			end)
-			vim.keymap.set("i", "<A-w>", function()
-				require("neocodeium").accept_word()
-			end)
-			vim.keymap.set("i", "<A-l>", function()
-				require("neocodeium").accept_line()
-			end)
-			vim.keymap.set("i", "<A-e>", function()
-				require("neocodeium").cycle_or_complete()
-			end)
-			vim.keymap.set("i", "<A-r>", function()
-				require("neocodeium").cycle_or_complete(-1)
-			end)
-			vim.keymap.set("i", "<A-c>", function()
-				require("neocodeium").clear()
-			end)
-		end,
-	}, ]]
+    -- LUA ALTERNATIVE
+    -- "monkoose/neocodeium"
 }
