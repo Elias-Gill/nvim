fa6db58a7a498ee48d36e423c0df3c4ef0c1ab2b
Elias Gill <eliasgill42@gmail.com>
2024-09-12 23:37:32 -0400

    fix: oldworld integrations:

--- lua/oldworld/groups/integrations/indent_blankline.lua
+++ lua/oldworld/groups/integrations/indent_blankline.lua
@@ -1,6 +1,6 @@
 local p = require("oldworld.palette")
 
 return {
-    IblIndent = { fg = p.gray2 },
-    IblScope = { fg = p.gray3 },
+    ["@ibl.indent.char.1"] = { fg = p.gray1 },
+    ["@ibl.scope.char.1"] = { fg = p.gray4 },
 }
--- lua/oldworld/highlights.lua
+++ lua/oldworld/highlights.lua
@@ -6,14 +6,12 @@ local M = {}
 
 local integrations = {
 	cmp = require("oldworld.groups.integrations.cmp"),
-    bqf = require("oldworld.groups.integrations.bqf"),
-	gitsigns = require("oldworld.groups.integrations.gitsigns"),
+	bqf = require("oldworld.groups.integrations.bqf"),
 	indent_blankline = require("oldworld.groups.integrations.indent_blankline"),
 	lazy = require("oldworld.groups.integrations.lazy"),
 	lsp = require("oldworld.groups.integrations.lsp"),
 	markdown = require("oldworld.groups.integrations.markdown"),
 	mason = require("oldworld.groups.integrations.mason"),
-	telescope = require("oldworld.groups.integrations.telescope"),
 	treesitter = require("oldworld.groups.integrations.treesitter"),
 }
 
@@ -24,10 +22,13 @@ local function load_highlights(highlights)
 end
 
 function M.setup()
-	load_highlights(editor_highlights)
-	load_highlights(syntax_highlights)
-	for _, plugin in ipairs(integrations) do
-        load_highlights(plugin.highlights)
+    load_highlights(editor_highlights)
+    load_highlights(syntax_highlights)
+
+    for _, plugin in pairs(integrations) do
+        for group_name, group_settings in pairs(plugin) do
+            vim.api.nvim_set_hl(0, group_name, group_settings)
+        end
     end
 end
 
