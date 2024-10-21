347416e75b6ac358d6f9dd2f640b6c0746f7dfd8
Elias Gill <eliasgill42@gmail.com>
2024-10-21 00:24:33 -0300

    fix: updating neovim api

--- lua/elias/plugins/ui/lualine.lua
+++ lua/elias/plugins/ui/lualine.lua
@@ -127,7 +127,7 @@ return {
 				local msg = "No Active Lsp"
 				local prompt = ""
 				local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-				local clients = vim.lsp.get_active_clients()
+				local clients = vim.lsp.get_clients()
 
 				if next(clients) == nil then
 					return msg
