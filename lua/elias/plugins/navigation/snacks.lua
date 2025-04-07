531ca7b4d1a34d23cd12310997393ae9cdecbd22
Elias Gill <eliasgill42@gmail.com>
2025-04-07 10:06:48 -0300

    migrating into snacks neovim

---
+++ lua/elias/plugins/navigation/snacks.lua
@@ -0,0 +1,24 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		bigfile = { enabled = true },
+		input = { enabled = true },
+		picker = { 
+            enabled = true,
+            icons = { enabled = false, },
+            exclude = { "go/", "*.class", "VirtualBox/", "node_modules/" },
+        },
+        quickfile = { enabled = true },
+	},
+
+    keys = {
+        { "<C-p>", function() Snacks.picker.files() end, desc = "Find files" },
+        { "<leader>fr", function() Snacks.picker.resume() end, desc = "Resume last search" },
+        { "<leader>ff", function() Snacks.picker.grep() end, desc = "Fuzzy grep" },
+        { "<leader>fo", function() Snacks.picker.rescent() end, desc = "Find History" },
+        { "<leader>fm", function() Snacks.picker.pick() end, desc = "Picker menu" }, --
+    }
+}
