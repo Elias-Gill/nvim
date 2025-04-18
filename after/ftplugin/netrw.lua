3fd23763769495295b498858088df3eb8327ea7b
Elias Gill <eliasgill42@gmail.com>
2025-04-18 15:23:34 -0300

    fix: netrw mappings

--- after/ftplugin/netrw.lua
+++ after/ftplugin/netrw.lua
@@ -1,6 +1,5 @@
 local opts = { remap = true, silent = true, buffer = true }
 
-vim.keymap.set("n", "v", "mf", opts)
 vim.keymap.set("n", "a", "Ccd%:w<CR>", opts)
 
 vim.keymap.set('n', '<C-r>', '<C-l>', { noremap = true, silent = true, buffer = true })
