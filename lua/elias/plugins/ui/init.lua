5bf9a24858112d71f762286a71573431f85f985f
Elias Gill <eliasgill42@gmail.com>
2025-06-26 12:32:33 -0300

    refactor: cleaning unnuesed config

--- lua/elias/plugins/ui/init.lua
+++ lua/elias/plugins/ui/init.lua
@@ -1,5 +1,27 @@
 return {
+    -- icons
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
-	-- "thimc/gruber-darker.nvim",
-    -- "aktersnurra/no-clown-fiesta.nvim"
+
+    -- colorizer
+	{
+		"chrisbra/colorizer",
+		cmd = "ColorToggle",
+	},
+
+	--ayudas de indentacion
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		event = "VeryLazy",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {
+			indent = { char = "┆" },
+			scope = {
+				show_start = false,
+				show_end = false,
+			},
+			exclude = { filetypes = { "dashboard", "vim", "markdown", "txt" } },
+		},
+	},
 }
